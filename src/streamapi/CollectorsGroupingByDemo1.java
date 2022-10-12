351ad79c95c4add33978151b3a73001963549d74
Nandivaleamol <nandivaleamol@gmail.com>
2022-10-12 15:11:36 +0530

    Added date : 12th oct 2022 Java 8 new stream api features

---
+++ src/streamapi/CollectorsGroupingByDemo1.java
@@ -0,0 +1,84 @@
+package streamapi;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CollectorsGroupingByDemo1 {
+    public static void main(String[] args) {
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("Andy","London",23));
+        personList.add(new Person("Rocky","India",25));
+        personList.add(new Person("Johny","USA",23));
+        personList.add(new Person("Danny","London",25));
+        personList.add(new Person("Mark","London",25));
+        personList.add(new Person("Munna","India",23));
+        personList.add(new Person("Jacky","India",26));
+
+
+        System.out.println("----------------GroupingByCity----------------");
+        Map<String, List<Person>> groupingByCity = personList.stream().collect(Collectors.groupingBy(Person::getCity));
+        groupingByCity.forEach((city,persons)->{
+            System.out.println(city);
+            System.out.println(persons);
+        });
+
+        System.out.println();
+        System.out.println("----------------GroupingByAge----------------");
+        Map<Integer, List<Person>> groupingByAge = personList.stream().collect(Collectors.groupingBy(Person::getAge));
+        groupingByAge.forEach((age, persons)->{
+            System.out.println(age);
+            System.out.println(persons);
+        });
+    }
+}
+
+class Person{
+    private String name;
+    private String city;
+    private int age;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public Person() {
+    }
+
+    public Person(String name, String city, int age) {
+        this.name = name;
+        this.city = city;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/streamapi/StreamImpForEachLoop.java
@@ -0,0 +1,60 @@
+package streamapi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class StreamImpForEachLoop {
+    public static void main(String[] args) {
+        IntStream.of(1,2,3,5,6,7,8,9).forEach(System.out::println);
+
+        System.out.println("-------------square of list------------");
+        List<Integer> numberList= Arrays.asList(1,3,5,6,2,8,5,4,9);
+
+        List<Integer> squareList = numberList.stream().map(n -> n * n).distinct().collect(Collectors.toList());
+        squareList.forEach(System.out::println);
+
+        System.out.println("--------------filter()--------------");
+        List<String> stringList = new ArrayList<>();
+        stringList.add("John");
+        stringList.add("");
+        stringList.add("Danny");
+        stringList.add("Locky");
+        stringList.add("Chanty");
+        stringList.add("");
+        stringList.add("Donya");
+        stringList.add("");
+        stringList.add("Narya");
+        stringList.add("");
+
+        long count = stringList.stream().filter(list -> list.isEmpty()).count();
+        System.out.println("Empty list objects : "+count);
+
+        System.out.println("-----------iterate()-----------");
+        IntStream.iterate(5,n->n+5).limit(10).forEach(System.out::println);
+
+        System.out.println("----------Sorting list ascending order-----------");
+        numberList.stream().sorted().forEach(System.out::println);
+
+        System.out.println("----------Sorting list descending order-----------");
+        numberList.stream().sorted((n1,n2)-> n2.compareTo(n1)).forEach(System.out::println);
+
+        System.out.println("-----------Collectors------------");
+        stringList.stream().filter(name->!name.isEmpty()).collect(Collectors.toList()).forEach(System.out::println);
+        System.out.println("-----------------------------------");
+        String combinedStrings = stringList.stream().filter(name -> !name.isEmpty()).collect(Collectors.joining(","));
+        System.out.println(combinedStrings);
+
+        System.out.println("---------mapTo()---------");
+        IntSummaryStatistics summeryStatistics = numberList.stream().mapToInt(number -> number).summaryStatistics();
+        System.out.println("Average : "+summeryStatistics.getAverage());
+        System.out.println("Sum : "+summeryStatistics.getSum());
+        System.out.println("Max Number : "+summeryStatistics.getMax());
+        System.out.println("Min Number : "+summeryStatistics.getMin());
+        System.out.println("Total numbers : "+summeryStatistics.getCount());
+
+    }
+}
