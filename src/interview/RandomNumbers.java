099fceb46ff1ac6efa4704ecb3479621c12ff094
Amol Nandivale <nandivaleamol@gmail.com>
2023-01-15 21:47:06 +0530

    stream api

--- src/interview/RandomNumbers.java
+++ src/interview/RandomNumbers.java
@@ -8,9 +8,16 @@ import java.util.stream.IntStream;
 public class RandomNumbers {
     public static void main(String[] args) {
 
+        // 1st approach
         Random random = new Random();
         IntStream limit = random.ints().limit(10);
-
         limit.forEach(System.out::println);
+
+        System.out.println("----------------------");
+
+        // 2nd approach
+        IntStream intStream = IntStream.generate(()->new Random().nextInt());
+        intStream.limit(10).forEach(System.out::println);
+
     }
 }
