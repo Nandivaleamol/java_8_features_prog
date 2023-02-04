1bdc2a2bca8fd3327ba55ccc2e7d60d180f93b5f
Amol Nandivale <nandivaleamol@gmail.com>
2023-02-04 18:47:16 +0530

    stream api

---
+++ src/interview/GroupsByWordsLength.java
@@ -0,0 +1,20 @@
+package interview;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+// Problem Statement: Write a java program to group the words by length using java 8 features
+public class GroupsByWordsLength {
+    public static void main(String[] args) {
+
+        String str = "i have a sentence with technical words";
+
+        String[] strArr = str.trim().split(" ");
+
+        Map<Integer,List<String>> mapLength=Arrays.stream(strArr).collect(Collectors.groupingBy(String::length));
+        mapLength.forEach((integer, strings) -> System.out.println(integer+"="+strings));
+
+    }
+}
